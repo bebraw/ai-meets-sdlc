1e5b035520e56d9e158913f8337d95ed42309f66
Juho Vepsäläinen <bebraw@gmail.com>
2026-06-18 15:47:08 +0300

    Add speaker detail anchors

--- site/dataSources.ts
+++ site/dataSources.ts
@@ -7,17 +7,24 @@ function init() {
     scheduleItems: () => scheduleItems,
     speakerItems: () =>
       scheduleItems.flatMap((item) =>
-        (item.talks ?? []).map((talk) => ({
-          ...talk.speaker,
-          anchor: item.anchor,
-          scheduleHref: item.scheduleHref,
-          time: talk.time,
-          sessionTitle: item.title,
-          talk: {
-            title: talk.title,
-            abstract: talk.abstract,
-          },
-        })),
+        (item.talks ?? []).map((talk) => {
+          const speakerAnchor = getSpeakerAnchor(talk.speaker.name);
+
+          return {
+            ...talk.speaker,
+            anchor: speakerAnchor,
+            anchorHref: `#${speakerAnchor}`,
+            speakerHref: `/speakers/#${speakerAnchor}`,
+            sessionAnchor: item.anchor,
+            scheduleHref: item.scheduleHref,
+            time: talk.time,
+            sessionTitle: item.title,
+            talk: {
+              title: talk.title,
+              abstract: talk.abstract,
+            },
+          };
+        }),
       ),
   };
 }
@@ -25,9 +32,22 @@ function init() {
 function getScheduleItems() {
   return schedule.items.map((item) => {
     const anchor = getSessionAnchor(item);
+    const talks = item.talks?.map((talk) => {
+      const speakerAnchor = getSpeakerAnchor(talk.speaker.name);
+
+      return {
+        ...talk,
+        speaker: {
+          ...talk.speaker,
+          anchor: speakerAnchor,
+          speakerHref: `/speakers/#${speakerAnchor}`,
+        },
+      };
+    });
 
     return {
       ...item,
+      talks,
       anchor,
       anchorHref: `#${anchor}`,
       scheduleHref: `/schedule/#${anchor}`,
@@ -39,6 +59,10 @@ function getSessionAnchor(item) {
   return `session-${item.time.slice(0, 5).replace(":", "")}-${slugify(
     item.title,
   )}`;
+}
+
+function getSpeakerAnchor(name) {
+  return slugify(name);
 }
 
 function slugify(value) {
